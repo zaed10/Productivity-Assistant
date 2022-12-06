005e80c1518947198a6b824ae93f93ea94cf5dfd
jeffwang <wangjeffrey30@gmail.com>
2022-12-06 15:12:49 -0500

    module info

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.productivityassistant {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.productivityassistant to javafx.fxml;
-    exports com.example.productivityassistant;
-}
