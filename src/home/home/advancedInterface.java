c7242843e303caae7bf676d9ecc9025ff75dadfa
jeffwang <wangjeffrey30@gmail.com>
2022-12-06 23:39:15 -0500

    advancedInterface (adapter design pattern)

---
+++ src/home/home/advancedInterface.java
@@ -0,0 +1,9 @@
+package home;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public interface advancedInterface {
+    void switchToVbox(Stage primaryStage);
+    void switchToStage(VBox box);
+}
